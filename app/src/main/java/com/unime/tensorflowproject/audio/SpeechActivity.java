998d9e7e4e3294ef7880b016acc8dbfe071b4419
lockits <miragliotta.giuseppe7@gmail.com>
2017-10-18 11:34:54 +0200

    second commit for speech recognition functionality

--- app/src/main/java/com/unime/tensorflowproject/audio/SpeechActivity.java
+++ app/src/main/java/com/unime/tensorflowproject/audio/SpeechActivity.java
@@ -2,7 +2,9 @@ package com.unime.tensorflowproject.audio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
@@ -10,11 +12,49 @@ import android.widget.Toast;
 import com.unime.tensorflowproject.R;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class SpeechActivity extends AppCompatActivity {
 
     private static final String TAG = "SpeechActivity";
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_speech);
+//        getSpeechInput();
+//    }
+//
+//    public void getSpeechInput() {
+//
+//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+//
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            Log.d(TAG, "getSpeechInput: inside if");
+//            startActivityForResult(intent, 10);
+//        } else {
+//            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        Log.d(TAG, "onActivityResult: here");
+//        switch (requestCode) {
+//            case 10:
+//                if (resultCode == RESULT_OK && data != null) {
+//                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                    Toast.makeText(this, result.get(0), Toast.LENGTH_SHORT).show();
+//                    Log.d(TAG, "onActivityResult: " + result.get(0));
+//                }
+//                break;
+//        }
+//    }
+
+    private SpeechRecognizer mSpeechRecognizer;
+    private Intent mSpeechRecognizerIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,138 +62,99 @@ public class SpeechActivity extends AppCompatActivity {
         setContentView(R.layout.activity_speech);
         getSpeechInput();
     }
-
     public void getSpeechInput() {
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+                this.getPackageName());
 
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        SpeechRecognitionListener listener = new SpeechRecognitionListener();
+        mSpeechRecognizer.setRecognitionListener(listener);
 
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            Log.d(TAG, "getSpeechInput: inside if");
-            startActivityForResult(intent, 10);
-        } else {
-            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
-        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Log.d(TAG, "onActivityResult: here");
-        switch (requestCode) {
-            case 10:
-                if (resultCode == RESULT_OK && data != null) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    Toast.makeText(this, result.get(0), Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "onActivityResult: " + result.get(0));
-                }
-                break;
-        }
+    protected void onResume() {
+        super.onResume();
+        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
     }
 
-//    private SpeechRecognizer mSpeechRecognizer;
-//    private Intent mSpeechRecognizerIntent;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_speech);
-//        getSpeechInput();
-//    }
-//    public void getSpeechInput() {
-//        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-//        mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-//        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-//                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-//        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
-//                this.getPackageName());
-//
-//        SpeechRecognitionListener listener = new SpeechRecognitionListener();
-//        mSpeechRecognizer.setRecognitionListener(listener);
-//
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        if (mSpeechRecognizer != null)
-//        {
-//            mSpeechRecognizer.destroy();
-//        }
-//        super.onDestroy();
-//    }
-//
-//
-//
-//    protected class SpeechRecognitionListener implements RecognitionListener
-//    {
-//
-//        @Override
-//        public void onBeginningOfSpeech()
-//        {
-//            Log.d(TAG, "onBeginingOfSpeech");
-//        }
-//
-//        @Override
-//        public void onBufferReceived(byte[] buffer)
-//        {
-//
-//        }
-//
-//        @Override
-//        public void onEndOfSpeech()
-//        {
-//            Log.d(TAG, "onEndOfSpeech");
-//        }
-//
-//        @Override
-//        public void onError(int error)
-//        {
-//            //mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
-//
-//            Log.d(TAG, "error = " + error);
-//        }
-//
-//        @Override
-//        public void onEvent(int eventType, Bundle params)
-//        {
-//
-//        }
-//
-//        @Override
-//        public void onPartialResults(Bundle partialResults)
-//        {
-//
-//        }
-//
-//        @Override
-//        public void onReadyForSpeech(Bundle params)
-//        {
-//            Log.d(TAG, "onReadyForSpeech");
-//        }
-//
-//        @Override
-//        public void onResults(Bundle results)
-//        {
-//            Log.d(TAG, "onResults");
-//            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-//           if(matches.get(0) != null) {
-//               Toast.makeText(SpeechActivity.this, matches.get(0), Toast.LENGTH_SHORT).show();
-//           }
-//
-//            // matches are the return values of speech recognition engine
-//            // Use these values for whatever you wish to do
-//        }
-//
-//        @Override
-//        public void onRmsChanged(float rmsdB)
-//        {
-//        }
-//    }
+    @Override
+    protected void onDestroy() {
+        if (mSpeechRecognizer != null)
+        {
+            mSpeechRecognizer.destroy();
+        }
+        super.onDestroy();
+    }
+
+
+
+    protected class SpeechRecognitionListener implements RecognitionListener
+    {
+
+        @Override
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginingOfSpeech");
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer)
+        {
+
+        }
+
+        @Override
+        public void onEndOfSpeech()
+        {
+            Log.d(TAG, "onEndOfSpeech");
+        }
+
+        @Override
+        public void onError(int error)
+        {
+            //mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+
+            Log.d(TAG, "error = " + error);
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params)
+        {
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults)
+        {
+
+        }
+
+        @Override
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+
+        @Override
+        public void onResults(Bundle results)
+        {
+            Log.d(TAG, "onResults");
+            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+           if(matches.get(0) != null) {
+               Toast.makeText(SpeechActivity.this, matches.get(0), Toast.LENGTH_SHORT).show();
+           }
+
+            // matches are the return values of speech recognition engine
+            // Use these values for whatever you wish to do
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB)
+        {
+        }
+    }
 }
